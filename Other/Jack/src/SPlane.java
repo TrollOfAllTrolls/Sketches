e688fe8714f17b712f5760c63eaed7f53124ddcd
Mrfuzzy <league@WTS8.attlocal.net>
2016-01-07 18:45:48 -0800

    Stuff
    kk

--- Other/Jack/src/SPlane.java
+++ Other/Jack/src/SPlane.java
@@ -4,14 +4,17 @@ public class SPlane {
 	public static void main(String[] args) {
 		int PoD;
 		int VSnakes = 0;
-		int numberOfPassengers;
+		int Viciousness = 0;
+		int numberOfPassengers = 366;
 		ArrayList<SSnake> Snakes = new ArrayList<SSnake>();
 		for (int i = 0; i < 100; i++) {
-			Snakes.add(new SSnake(SRandomness.random, SRandomness.TF));
+			Snakes.add(new SSnake(new SRandomness().rN(), new SRandomness().rTF()));
+			Viciousness = Viciousness + Snakes.get(i).getViciousness();
 			if (Snakes.get(i).isVenemous()) {
 				VSnakes = VSnakes + 1;
 			}
 		}
-		PoD = 0;
+		PoD = VSnakes * Viciousness / numberOfPassengers;
+		System.out.println("The Probability of Death is " + PoD);
 	}
 }
--- Other/Jack/src/SRandomness.java
+++ Other/Jack/src/SRandomness.java
@@ -5,12 +5,9 @@ public class SRandomness {
 	static int random;
 	static boolean TF;
 
-	public static void main(String[] args) {
-
-	}
-
 	public int rN() {
 		random = r.nextInt(10) + 1;
+
 		return random;
 	}
 
