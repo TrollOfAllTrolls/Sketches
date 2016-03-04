94be9eb1c82973f89321044bdc1c6330f2c7b911
Kendra Kleber <league@iMac.attlocal.net>
2016-03-03 18:24:52 -0800

    s

--- CookieOrder/src/CookieOrderTest.java
+++ CookieOrder/src/CookieOrderTest.java
@@ -20,7 +20,7 @@ public class CookieOrderTest {
 	 * 
 	 * (d) When you are done, uncomment this test and run it to see if your class works.
 	 */
-	
+
 	@Test
 	public void testClimbInfo() {
 		// test constructor
@@ -53,52 +53,51 @@ public class CookieOrderTest {
 	}
 
 	/* 3. Add a method to MasterOrder that allows you to add a CookieOrder to it's List. */
-//	@Test
-//	public void testAddCookieOrderConstructor() throws Exception {
-//		MasterOrder masterOrder = new MasterOrder();
-//		masterOrder.addOrder(new CookieOrder("chocolate chip", 3));
-//		assertEquals(1, masterOrder.getOrders().size());
-//
-//		assertEquals(3, masterOrder.getOrders().get(0).getNumBoxes());
-//		assertEquals("chocolate chip", masterOrder.getOrders().get(0).getVariety());
-//	}
+	@Test
+	public void testAddCookieOrderConstructor() throws Exception {
+		MasterOrder masterOrder = new MasterOrder();
+		masterOrder.addOrder(new CookieOrder("Chocolate Chip", 3));
+		assertEquals(1, masterOrder.getOrders().size());
+
+		assertEquals(3, masterOrder.getOrders().get(0).getNumBoxes());
+		assertEquals("Chocolate Chip", masterOrder.getOrders().get(0).getVariety());
+	}
 
 	/*
 	 * PART A
 	 * 
-	 * The getTotalBoxes method computes and returns the sum of the number of boxes of all cookie orders. If there are no cookie orders in the master
-	 * order, the method returns 0.
+	 * The getTotalBoxes method computes and returns the sum of the number of boxes of all cookie orders. If there are
+	 * no cookie orders in the master order, the method returns 0.
 	 */
-//	@Test
-//	public void testGetTotalBoxes() throws Exception {
-//		MasterOrder goodies = new MasterOrder();
-//		goodies.addOrder(new CookieOrder("Chocolate Chip", 1));
-//		goodies.addOrder(new CookieOrder("Shortbread", 5));
-//		goodies.addOrder(new CookieOrder("Macaroon", 2));
-//		goodies.addOrder(new CookieOrder("Chocolate Chip", 3));
-//
-//		assertEquals(11, goodies.getTotalBoxes());
-//	}
+	@Test
+	public void testGetTotalBoxes() throws Exception {
+		MasterOrder goodies = new MasterOrder();
+		goodies.addOrder(new CookieOrder("Chocolate Chip", 1));
+		goodies.addOrder(new CookieOrder("Shortbread", 5));
+		goodies.addOrder(new CookieOrder("Macaroon", 2));
+		goodies.addOrder(new CookieOrder("Chocolate Chip", 3));
+
+		assertEquals(11, goodies.getTotalBoxes());
+	}
 
 	/*
 	 * PART B
 	 * 
-	 * The removeVariety method updates the master order by removing all of the cookie orders in which the variety of cookie matches the parameter
-	 * cookieVar. The master order may contain zero or more cookie orders with the same variety as cookieVar. The method returns the total number of
-	 * boxes removed from the master order.
+	 * The removeVariety method updates the master order by removing all of the cookie orders in which the variety of
+	 * cookie matches the parameter cookieVar. The master order may contain zero or more cookie orders with the same
+	 * variety as cookieVar. The method returns the total number of boxes removed from the master order.
 	 */
-//	@Test
-//	public void testRemoveVariety() throws Exception {
-//		MasterOrder goodies = new MasterOrder();
-//		goodies.addOrder(new CookieOrder("Chocolate Chip", 1));
-//		goodies.addOrder(new CookieOrder("Shortbread", 5));
-//		goodies.addOrder(new CookieOrder("Macaroon", 2));
-//		goodies.addOrder(new CookieOrder("Chocolate Chip", 3));
-//		goodies.addOrder(new CookieOrder("Macaroon", 7));
-//		
-//		assertEquals(4, goodies.removeVariety("Chocolate Chip"));
-//		assertEquals(0, goodies.removeVariety("Brownie"));
-//	}
+	@Test
+	public void testRemoveVariety() throws Exception {
+		MasterOrder goodies = new MasterOrder();
+		goodies.addOrder(new CookieOrder("Chocolate Chip", 1));
+		goodies.addOrder(new CookieOrder("Shortbread", 5));
+		goodies.addOrder(new CookieOrder("Macaroon", 2));
+		goodies.addOrder(new CookieOrder("Chocolate Chip", 3));
+		goodies.addOrder(new CookieOrder("Macaroon", 7));
+
+		assertEquals(4, goodies.removeVariety("Chocolate Chip"));
+		assertEquals(0, goodies.removeVariety("Brownie"));
+	}
 
 }
-
--- CookieOrder/src/MasterOrder.java
+++ CookieOrder/src/MasterOrder.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MasterOrder {
-	ArrayList<CookieOrder> Orders = new ArrayList();
+	ArrayList<CookieOrder> Orders = new ArrayList<CookieOrder>();
 
 	public ArrayList<CookieOrder> getCookieorders() {
 		return Orders;
@@ -11,8 +12,33 @@ public class MasterOrder {
 		Orders = cookieorders;
 	}
 
-	public Object getOrders() {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<CookieOrder> getOrders() {
+		return Orders;
+	}
+
+	public void addOrder(CookieOrder cookieOrder) {
+		Orders.add(cookieOrder);
+	}
+
+	public int getTotalBoxes() {
+		int numberBoxes = 0;
+		for (CookieOrder Cookies : Orders) {
+			numberBoxes = numberBoxes + Cookies.getNumBoxes();
+		}
+		return numberBoxes;
+
+	}
+
+	public int removeVariety(String v) {
+		Iterator<CookieOrder> Order = Orders.iterator();
+		int removedBoxes = 0;
+		while (Order.hasNext()) {
+			CookieOrder Cookiez = Order.next();
+			if (Cookiez.getVariety().equals(v)) {
+				removedBoxes = removedBoxes + Cookiez.getNumBoxes();
+				Order.remove();
+			}
+		}
+		return removedBoxes;
 	}
 }
